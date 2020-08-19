a9da01e4288dad6615ebd00a33fb7fc385bc6954
IryArkhy <37odri@gmail.com>
2020-08-19 19:01:17 +0300

    small fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import routes from '../../routes';
 
 const Header = () => {
   return (
-    <header sx={{ width: '100%', height: '10%', bg: 'primary' }}>
+    <header sx={{ width: '100%', height: '8vh', bg: 'primary' }}>
       <ul>
         <li>
           <NavLink exact to={routes.HOME_PAGE}>
