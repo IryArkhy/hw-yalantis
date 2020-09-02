fd9fc2bf964881b94926ce9c16f69b4a149ff19f
IryArkhy <37odri@gmail.com>
2020-09-02 21:07:09 +0300

    made some fixes

--- src/redux/loading/loadingReducer.js
+++ src/redux/loading/loadingReducer.js
@@ -1,4 +1,4 @@
-const loadingReducer = (state, { type }) => {
+const loadingReducer = (state = false, { type }) => {
   const matches = /(.*)_(SUCCESS|FAILURE)/.exec(type);
   if (!matches) return true;
   return false;
