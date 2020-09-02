b67f6eabc66fb019cd88e0b8691bed973dd3d3c0
IryArkhy <37odri@gmail.com>
2020-09-03 00:02:37 +0300

    validation schema with yup for NewProduct form component

---
+++ src/components/NewProductForm/NewProductForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './productForm.module.css';
+
+const NewProductForm = () => {
+  return (
+    <>
+      <h1> ⊹ ShopOnline</h1>
+    </>
+  );
+};
+
+export default NewProductForm;
