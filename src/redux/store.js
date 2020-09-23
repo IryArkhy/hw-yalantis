f379fd8fe251fc3e75b9fd5b52725bd00586d85e
IryArkhy <37odri@gmail.com>
2020-09-23 13:17:42 +0300

    .gitignore .env

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,23 +4,9 @@ import createSagaMiddleware from 'redux-saga';
 import thunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
 import rootReducer from './rootReducer';
-import rootSaga from './rootSaga';
 
 const middleware = [thunk];
 const enhuncer = composeWithDevTools(applyMiddleware(...middleware));
 
 export const store = createStore(rootReducer, enhuncer);
 export const persistor = persistStore(store);
-
-// new --------
-const configureStore = (initialState = {}) => {
-  const sagaMiddleware = createSagaMiddleware();
-  const storage = createStore(
-    rootReducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(sagaMiddleware)),
-  );
-  sagaMiddleware.run(rootSaga);
-
-  return store;
-};
