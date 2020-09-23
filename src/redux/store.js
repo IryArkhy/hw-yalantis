06a948643ebe8953bd0195e077c6a863699fccdf
IryArkhy <37odri@gmail.com>
2020-09-23 12:42:07 +0300

    fix in UserProfile.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,26 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 import thunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
 import rootReducer from './rootReducer';
+import rootSaga from './rootSaga';
 
 const middleware = [thunk];
 const enhuncer = composeWithDevTools(applyMiddleware(...middleware));
 
 export const store = createStore(rootReducer, enhuncer);
 export const persistor = persistStore(store);
+
+// new --------
+const configureStore = (initialState = {}) => {
+  const sagaMiddleware = createSagaMiddleware();
+  const storage = createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(sagaMiddleware)),
+  );
+  sagaMiddleware.run(rootSaga);
+
+  return store;
+};
